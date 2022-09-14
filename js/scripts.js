1aa5fbf639b7bc3dddf4e3804b3a8e400c6e3d5c
TrondStolsvik <trondstolsvik@gmail.com>
2022-09-14 20:25:45 +0200

    pseudo-coding the js portion

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-function HelloWorld() {
-  alert("Hello World!");
+function getValues() {
+  //Declare variable doubleValue and assign the value from ID doubleValue on the page
+  //Declare variable bubbleValue and assign the value from ID bubbleValue on the page
+  //Parse to integers
+  //Validate that the values are integers and if-else to test the truth of the values
+  //Inside if-else call doubleBubble()
+  //Declare variable dbData and set to the value of doubleBubble(doubleValue, bubbleValue)
+  //Call displayData(dbData)
+  //Else if either of doubleValue and bubbleValue are not numbers
+  //display alert prompting the user to enter integers
+}
+
+function doubleBubble(dValue, bValue) {
+  //123
 }
